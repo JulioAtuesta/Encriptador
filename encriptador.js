1a5a54a16305b60592bacb25508af447056cf449
JulioAtuesta <jcatuestav@uqvirtual.edu.co>
2023-10-05 14:58:53 -0500

    diccionario de encriptacion

--- encriptador.js
+++ encriptador.js
@@ -2,41 +2,35 @@ function main() {
     const submit = document.getElementById('encriptador__input-encriptar');
     submit.addEventListener('click',encriptar); 
 }
-
+/*
+!no uses codigo que no entiendas
+*/
 function encriptar(){
+    const dickyDick = {a:'ai', e:'enter', i:'imes', o:'ober', u:'ufat'}
     let textoEncriptado =''
   console.log('funcion encriptadora');
     const texto = document.getElementById('encriptador__input-text');
-  console.log(texto.value);
-  let a =validacion(texto.value);
+    const textoBase = texto.value;
+    if(textoBase === ""){
+        return (0)
+    }
+  console.log(textoBase); 
+  let a =validacion(textoBase);
   console.log(a);
-    if(a){
+    if(a){  // si, se peude cambiar, todavia no
         console.log('encriptando');
-        let textoEncriptado = ""
+        for (const key of Object.keys(dickyDick)){//recorrer el texto base y bucar las keys para cambiarlas por el value
+            textoEncriptado = textoBase.replace(key,dickyDick[key]);
+            console.log(textoEncriptado);
+        }
         mostrar(textoEncriptado)
     }      
 }
+
 function validacion(texto){
     console.log(typeof(texto) + texto);
     const invalidos = /[A-Z0-9ÁÉÍÓÚÜÑ¡¿@#$%^&*()_+={}[\]:;"'<>,.?\\/]/; // el - representa un espacio vacio cuando no se usa en un rango e.g [a-g]
     return !invalidos.test(texto);
-
-// function validacion(texto){
-//     const invalidos = /^[A-Z]*$/;
-//     // const invalidos = /^[A-Z0-9ÁÉÍÓÚÜÑ¡¿@#$%^&*()_+={}[\]:;"'<>,.?\\/ -]*$/;
-//     if (invalidos.test(texto)){
-//         return(false)
-//     }return(true)
-    
-    // return(!(texto.match(invalidos)!= null))    
-    
-    
-    // if(texto == "" || texto.match(invalidos)){
-    //         console.log('caracteres no validos');
-    //         return(false);
-    //     }else{ return(true);
-
-    //     }
 }
 
 function mostrar(textoEncriptado){
