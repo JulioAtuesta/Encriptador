78e89d118bceade24ff947cddc7573be12a8eae3
JulioAtuesta <jcatuestav@uqvirtual.edu.co>
2023-10-06 15:41:16 -0500

    cambio en el metodo para modificar la cadena

--- encriptador.js
+++ encriptador.js
@@ -2,40 +2,43 @@ function main() {
     const submit = document.getElementById('encriptador__input-encriptar');
     submit.addEventListener('click',encriptar); 
 }
-/*
-!no uses codigo que no entiendas
-*/
+
+
 function encriptar(){
     const dickyDick = {a:'ai', e:'enter', i:'imes', o:'ober', u:'ufat'}
-    let textoEncriptado =''
-  console.log('funcion encriptadora');
     const texto = document.getElementById('encriptador__input-text');
     const textoBase = texto.value;
     if(textoBase === ""){
         return (0)
     }
-  console.log(textoBase); 
-  let a =validacion(textoBase);
-  console.log(a);
-    if(a){  // si, se peude cambiar, todavia no
-        console.log('encriptando');
-        for (const key of Object.keys(dickyDick)){//recorrer el texto base y bucar las keys para cambiarlas por el value
-            textoEncriptado = textoBase.replace(key,dickyDick[key]);
-            console.log(textoEncriptado);
+    if(validacion(textoBase)){ 
+        let textoEncriptado ='';
+        for(let i=0; i<textoBase.length; i++){
+            let key = textoBase[i];
+            if(Object.keys(dickyDick).includes(key)){
+                    textoEncriptado += dickyDick[key];
+            } else{
+                textoEncriptado += key;
+            }
         }
         mostrar(textoEncriptado)
     }      
 }
 
+function desEncroptar(){
+
+}
+
+function copiarTexto(){}
+
 function validacion(texto){
-    console.log(typeof(texto) + texto);
+    console.log(typeof(texto) +' '+ texto);
     const invalidos = /[A-Z0-9ÁÉÍÓÚÜÑ¡¿@#$%^&*()_+={}[\]:;"'<>,.?\\/]/; // el - representa un espacio vacio cuando no se usa en un rango e.g [a-g]
     return !invalidos.test(texto);
 }
 
 function mostrar(textoEncriptado){
     const encriptado = document.getElementById('encriptador__output-texto');
-    console.log(textoEncriptado);
     encriptado.innerText = textoEncriptado;
 }
 
